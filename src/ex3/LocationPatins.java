5945f091b41275fb1532f8c995badbf274a53aad
20213136 <chehrazad.ouazzani@student.vinci.be>
2023-03-01 13:41:47 +0100

    Exo 3: not done

--- src/ex3/LocationPatins.java
+++ src/ex3/LocationPatins.java
@@ -1,34 +1,48 @@
 package ex3;
 
 import java.time.LocalTime;
+import java.util.HashMap;
 
 import static java.time.temporal.ChronoUnit.MILLIS;
 
 public class LocationPatins {
-	
-	public LocationPatins(int[] casiers) {
-	
-	}
+    HashMap<Integer, Integer> lesPaires;
 
-	// date1 < date2
-	private static double prix(LocalTime date1, LocalTime date2) {
-		// 1 euro par milliseconde (c'est assez cher en effet)
-		return MILLIS.between(date1, date2) ;
-	}
+    public LocationPatins(int[] casiers) {
+        lesPaires = new HashMap<Integer, Integer>();
+        int val;
+        for (int i = 0; i < casiers.length; i++) {
+            if (!lesPaires.containsKey(casiers[i]))
+                lesPaires.put(casiers[i], 0);
+            else {
+                val = lesPaires.get(casiers[i]);
+                val++;
+                lesPaires.put(casiers[i], val);
+            }
+        }
+    }
 
-	public int attribuerCasierAvecPatins(int pointure) {
-		if (pointure < 33 || pointure > 48)
-			throw new IllegalArgumentException();
-		LocalTime l = LocalTime.now();
-		
-		return 0;
-		//a compl�ter
+    // date1 < date2
+    private static double prix(LocalTime date1, LocalTime date2) {
+        // 1 euro par milliseconde (c'est assez cher en effet)
+        return MILLIS.between(date1, date2);
+    }
 
-	}
+    public int attribuerCasierAvecPatins(int pointure) {
+        if (pointure < 33 || pointure > 48)
+            throw new IllegalArgumentException();
+        LocalTime l = LocalTime.now();
+        int val = lesPaires.get(pointure);
+        val++;
+        lesPaires.put(pointure, val);
+        return 0;
+        //a compl�ter
 
-	public double libererCasier(int numeroCasier) {
-		//a completer
-		return 0;
-	}
+    }
+
+    public double libererCasier(int numeroCasier) {
+        //a completer
+        return 0;
+    }
 
 }
