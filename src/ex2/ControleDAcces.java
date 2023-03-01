aba1efb8e2881b1ea7497235be7b861f1af3bb2b
20213136 <chehrazad.ouazzani@student.vinci.be>
2023-03-01 13:22:44 +0100

    Exo 2 : DONE

--- src/ex2/ControleDAcces.java
+++ src/ex2/ControleDAcces.java
@@ -5,32 +5,35 @@ import java.util.HashSet;
 
 public class ControleDAcces {
 
-	private HashMap<Badge, Employe> employes;
-	private HashSet<Employe> present_batiment;
-	public ControleDAcces(){
-		employes = new HashMap<Badge, Employe>();
-		present_batiment = new HashSet<Employe>();
-	}
-	
-	// associe le badge � un employ�
-	public void donnerBadge (Badge b, Employe e){
-		employes.put(b,e);
-	}
-	
-	// met � jour les employ�s pr�sents dans le batiment
-	public void entrerBatiment (Badge b){
-		Employe emp = employes.get(b);
-	//	present_batiment.add();
-	}
+    private HashMap<Badge, Employe> employes;
+    private HashSet<Employe> present_batiment;
 
-	// met � jour les employ�s pr�sents dans le batiment
-	public void sortirBatiment (Badge b){
+    public ControleDAcces() {
+        employes = new HashMap<Badge, Employe>();
+        present_batiment = new HashSet<Employe>();
+    }
 
-	}
-	
-	// renvoie vrai si l'employ� est dans le batiment, faux sinon
-	public boolean estDansBatiment (Employe e){
-		return present_batiment.contains(e);
-	}
+    // associe le badge � un employé
+    public void donnerBadge(Badge b, Employe e) {
+        employes.put(b, e);
+    }
+
+    // met � jour les employ�s pr�sents dans le batiment
+    public void entrerBatiment(Badge b) {
+        if (employes.containsKey(b)) {
+            present_batiment.add(employes.get(b));
+        }
+    }
+
+    // met � jour les employ�s pr�sents dans le batiment
+    public void sortirBatiment(Badge b) {
+        if (employes.containsKey(b))
+            present_batiment.remove(employes.get(b));
+    }
+
+    // renvoie vrai si l'employ� est dans le batiment, faux sinon
+    public boolean estDansBatiment(Employe e) {
+        return present_batiment.contains(e);
+    }
 
 }
